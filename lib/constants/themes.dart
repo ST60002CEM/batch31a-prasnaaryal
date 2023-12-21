0534ce52eef744e88a18c401a78411a1fbe94e70
prasnaaryal <prasna101@gmail.com>
2023-12-21 12:35:47 +0545

    updated theme

---
+++ lib/constants/themes.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFF843667);
+const kSecondaryColor = Color(0xFF022238);
+const kThirdColor = Color(0xFFFFDCBC);
+const kLightBackground = Color(0xFFE8F6FB);
+const kWhiteColor = Colors.white;
+const kBlackColor = Colors.black;
