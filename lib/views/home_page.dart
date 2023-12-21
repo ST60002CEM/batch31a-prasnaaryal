c99de71ca2bf0d92b641f3da605a6e7e5655c104
prasnaaryal <prasna101@gmail.com>
2023-12-21 13:38:48 +0545

    created appbar

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,10 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:hamropasalmobile/constants/themes.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      key: _scaffoldKey,
+      appBar: AppBar(
+        backgroundColor: kSecondaryColor,
+        title: SvgPicture.asset(
+          'assets/general/store_brand_white.svg',
+          colorFilter: ColorFilter.mode(kWhiteColor, BlendMode.srcIn),
+          width: 180,
+        ),
+        actions: [
+          Padding(
+              padding: const EdgeInsets.only(right: 20),
+              child: IconButton(
+                  onPressed: () {}, icon: Icon(Icons.local_mall_outlined)))
+        ],
+        leading: IconButton(
+          onPressed: () async {
+            _scaffoldKey.currentState!.openDrawer();
+          },
+          icon: Icon(
+            Icons.menu,
+            color: Colors.white,
+          ),
+        ),
+      ),
+      drawer: const Drawer(),
+    );
   }
 }
