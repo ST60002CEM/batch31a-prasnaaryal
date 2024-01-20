b4e9d3f5b9a7b6cefcab2945c393bffe94a37c97
prasnaaryal <prasna101@gmail.com>
2024-01-20 15:18:47 +0545

    created detail page

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:hamropasalmobile/views/home_page.dart';
+import 'package:hamropasalmobile/views/detail_page.dart';
 
 void main() {
-  runApp(const ProviderScope(
-    child: MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
+  runApp(
+    ProviderScope(
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: DetailsPage(),
+      ),
     ),
-  ));
+  );
 }
